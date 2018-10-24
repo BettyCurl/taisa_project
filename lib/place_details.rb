1299f76a7b67f46102fc4c9bf9abc72eb0eec806
BettyCurl <peiyicurl@gmail.com>
2018-10-24 23:22:33 +0800

    Google Map API Library without handling sad cases

---
+++ lib/place_details.rb
@@ -0,0 +1,27 @@
+module DataCollection
+  class PlaceDetails
+
+    def initialize(place_id, url, place_details_data)
+      @details = place_details_data
+      @place_id = place_id
+      @details_url = url
+    end
+
+    def url
+      @details_url
+    end
+
+    def name
+      @details['result']['name']
+    end
+
+    def rating
+      @details['result']['rating']
+    end
+
+    def formatted_address
+      @details['result']['formatted_address']
+    end
+
+  end
+end
